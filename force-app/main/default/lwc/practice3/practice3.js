a6e156be107ad95c492daa4ec2ec762e6a3b32e1
Hyder Ayser <hyder.ayser@gmail.com>
2023-05-23 13:05:13 -0400

    Initial Commit

---
+++ force-app/main/default/lwc/practice3/practice3.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+
+export default class Practice3 extends LightningElement {
+  message = "Offer Received";
+
+  handleChange() {
+    console.log("User typed something");
+  }
+}
