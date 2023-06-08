86eecf49ac5033845c4bd65f497d3f882639abf1
Hyder Ayser <hyder.ayser@gmail.com>
2023-06-08 11:12:25 -0400

    Initial Commit

---
+++ force-app/main/default/lwc/foodItem/foodItem.js
@@ -0,0 +1,23 @@
+import { LightningElement, api } from "lwc";
+export default class FoodItem extends LightningElement {
+  // item = {
+  //     foodName: 'Pizza',
+  //     calorie: 2000
+  // }
+  @api
+  item;
+
+  handleClick() {
+    // handle click will fire a custom event
+    // called pick
+    // and parent component can listen to the event
+    // and take whatever action it needs to take
+
+    // this is how we create custom event
+    const myEvent = new CustomEvent("pick");
+    // this is how we dispatch a event
+    this.dispatchEvent(myEvent);
+    console.log("child dispatched an event called pick");
+    console.log(myEvent);
+  }
+}
